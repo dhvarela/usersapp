d161b2b364da87669b8eef265cf2e6188afdf664
Daniel <daniloviciv@hotmail.com>
2018-06-10 14:11:06 +0200

    first commit - skeleton with phpunit

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 10/06/18
+ * Time: 13:28
+ */
+echo "<h1>Hello</h1>";
+echo "<h2>Welcome to usersApp</h2>";
---
+++ tests/autoload.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 10/06/18
+ * Time: 13:30
+ */
+
+require_once __DIR__.'/../vendor/autoload.php';
